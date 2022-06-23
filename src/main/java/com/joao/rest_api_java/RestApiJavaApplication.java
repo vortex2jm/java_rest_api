e218856dca24758906e94a9597fd096f258109fd
vortex2jm <joaopaulomoura5328@gmail.com>
2022-06-23 19:47:33 -0300

    Initializing Java Rest API

---
+++ src/main/java/com/joao/rest_api_java/RestApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.joao.rest_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiJavaApplication.class, args);
+	}
+
+}
